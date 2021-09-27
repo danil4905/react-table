1cdbd1d93771a5e11eef9924201e2a6086ace633
Danil <conovalov.danil3441@gmail.com>
2021-09-27 11:22:26 +0400

    Реализовал фильтрацию

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -2,27 +2,33 @@ import Item from "../item/Item";
 import { useState, useEffect } from "react";
 import API from '../../utils/api';
 import { sorting } from '../../utils/sorting'
+import { filterContent} from '../../utils/filter';
 
 const Table = () => {
-    const [input,setInput] = useState('')
-    const [sortType,setSortType] = useState('title')
-    const [filterType,setFilterType] = useState('');
-    console.log(sortType)
-    const [contents, setContents] = useState(null)
+    const [input, setInput] = useState('')
+    const [sortType, setSortType] = useState('title')
+    const [filterType, setFilterType] = useState('title');
+    const [filterParam,setFilterParam] = useState('');
+    let [contents, setContents] = useState(null)
+    const [filtered, setFiltered] = useState(contents);
     useEffect(() => {
         API.get('contents').then((res) => {
             setContents(sorting(res.data, sortType));
         });
     }, []);
-    function sortItems (param) {
+    function sortItems(param) {
         setSortType(param)
-        setContents(sorting(contents,param))
+        setContents(sorting(contents, param))
     }
-    function setTitleDisabled () {
-        return filterType === 'title'?true:false
+    function setTitleDisabled() {
+        return filterType === 'title' ? true : false
     }
-    function setCountDisabled () {
+    function setCountDisabled() {
         return filterType !== 'title' ? true : false
+    }
+    function showFilter(value) {
+        setInput(value);
+        setFiltered(filterContent(contents, filterType, filterParam, value))
     }
     if (contents === null) return (
         <main>
@@ -33,18 +39,18 @@ const Table = () => {
         return (
             <main className='main'>
                 <div className='filter'>
-                    <select name="first" id="">
-                        <option value="title" onClick={() => setFilterType('title')}>Название</option>
-                        <option value="count" onClick={() => setFilterType('count')}>Количество</option>
-                        <option value="distance" onClick={() => setFilterType('distance')}>Дистанция</option>
+                    <select name="first" value={filterType} id="" onChange={(el) => setFilterType(el.target.value)}>
+                        <option value="title" >Название</option>
+                        <option value="count" >Количество</option>
+                        <option value="distance" >Дистанция</option>
                     </select>
-                    <select name="second" id="">
+                    <select name="second" id="" onChange={(el) => setFilterParam(el.target.value)}>
                         <option value="=" disabled={setTitleDisabled()}>равно</option>
                         <option value="include" disabled={setCountDisabled()}>содержит</option>
                         <option value=">" disabled={setTitleDisabled()}>больше</option>
                         <option value="<" disabled={setTitleDisabled()}>меньше</option>
                     </select>
-                    <input type="text" value={input} onChange={(el) => setInput(el.target.value)} />
+                    <input type="text" value={input} onChange={(el) => showFilter(el.target.value)} />
                 </div>
                 <ul className='names'>
                     <li className='names__item' onClick={() => sortItems('title')}>Название</li>
@@ -53,7 +59,8 @@ const Table = () => {
                     <li className='names__item' onClick={() => sortItems('distance')}>Дистанция</li>
                 </ul>
                 <div className='content'>
-                    {contents.map((el, index) => <Item id={index} title={el.title} date={el.date} count={el.count} distance={el.distance} />)}
+                    {input=== ''?contents.map((el, index) => <Item id={index} title={el.title} date={el.date} count={el.count} distance={el.distance} />)
+                        : filtered.map((el, index) => <Item id={index} title={el.title} date={el.date} count={el.count} distance={el.distance} />)}
                 </div>
             </main>
         )
--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,43 +1,44 @@
-export const filter = (content, paramFirst, paramSecond, input) => {
-  if (paramFirst === "title") {
-    if (paramSecond === "include") {
-    }
-  }
-  switch (paramFirst) {
-    case "title":
-      return content.filter(function (v) {
-        return v.title.includes(input);
-      });
-    case "count": {
-      if (paramSecond === "=")
+export const filterContent = (content, paramFirst, paramSecond, input) => {
+  console.log(input);
+  if (content === null) {
+    return content;
+  } else {
+    switch (paramFirst) {
+      case "title":
         return content.filter(function (v) {
-          return v.count === input;
+          return v.title.includes(input);
         });
-      if (paramSecond === ">")
-        return content.filter(function (v) {
-          return v.count > input;
-        });
-      else
-        return content.filter(function (v) {
-          return v.count < input;
-        });
-    }
+      case "count": {
+        if (paramSecond === "=")
+          return content.filter(function (v) {
+            return v.count * 1 === input * 1;
+          });
+        if (paramSecond === ">")
+          return content.filter(function (v) {
+            return v.count * 1 > input * 1;
+          });
+        else
+          return content.filter(function (v) {
+            return v.count * 1 < input * 1;
+          });
+      }
 
-    case "distance": {
-      if (paramSecond === "=")
-        return content.filter(function (v) {
-          return v.distance === input;
-        });
-      if (paramSecond === ">")
-        return content.filter(function (v) {
-          return v.distance > input;
-        });
-      else
-        return content.filter(function (v) {
-          return v.distance < input;
-        });
+      case "distance": {
+        if (paramSecond === "=")
+          return content.filter(function (v) {
+            return v.distance * 1 === input * 1;
+          });
+        if (paramSecond === ">")
+          return content.filter(function (v) {
+            return v.distance * 1 > input * 1;
+          });
+        else
+          return content.filter(function (v) {
+            return v.distance * 1 < input * 1;
+          });
+      }
+      default:
+        return content;
     }
-    default:
-      return content;
   }
 };
